50377e737924aeb9738d107dffa7b1607df3ce12
DmytroBaranov <dbr@sitecore.net>
2017-12-18 12:40:32 +0200

    Gets Profile Key Value from historial interactions.

--- src/Sitecore.Support.93470/Analytics/Rules/Conditions/ProfileCondition.cs
+++ src/Sitecore.Support.93470/Analytics/Rules/Conditions/ProfileCondition.cs
@@ -1,10 +1,15 @@
 ﻿namespace Sitecore.Support.Analytics.Rules.Conditions
 {
   using System;
+  using System.Linq;
   using Sitecore.Diagnostics;
   using Sitecore.Rules;
   using Sitecore.Rules.Conditions;
   using Sitecore.Analytics;
+  using Sitecore.Analytics.Data;
+  using Sitecore.Analytics.Tracking;
+
+
 
   /// <summary>Defines the when subitem of class.</summary>
   /// <typeparam name="T">The rule context.</typeparam>
@@ -92,8 +97,7 @@
     {
       Assert.ArgumentNotNull(ruleContext, "ruleContext");
       Assert.IsNotNull(Tracker.Current, "Tracker.Current is not initialized");
-      Assert.IsNotNull(Tracker.Current.Session, "Tracker.Current.Session is not initialized");
-      Assert.IsNotNull(Tracker.Current.Session.Interaction, "Tracker.Current.Session.Interaction is not initialized");
+      Assert.IsNotNull(Tracker.Current.Session, "Tracker.Current.Session is not initialized");     
 
       double conditionValue;
       if (!double.TryParse(this.Value, out conditionValue))
@@ -152,14 +156,43 @@
 
       var key = item.Name;
 
-      var profile = Tracker.Current.Session.Interaction.Profiles[parent.Name];
-
-      if (profile == null)
+      var interaction = Tracker.Current.Session.Interaction;
+      if (interaction != null && (interaction.Pages.Count() > 1 || interaction.CurrentPage.Url.Path != null))
       {
-        return 0;
-      }
+        var profile = interaction.Profiles[parent.Name];
 
-      return profile[key];
+        if (profile == null)
+        {
+          return 0;
+        }
+
+        return profile[key];
+      }
+      else
+      {
+        var history = Tracker.Current.Contact.LoadHistorycalData(1);
+        if (history == null || history.Count<IInteractionData>() == 0)
+        {
+          return 0;
+        }
+        var historyInteraction = history.FirstOrDefault();
+        if (historyInteraction == null)
+        {
+          return 0;
+        }
+        var interactionProfiles = historyInteraction.InteractionProfiles;
+        if (interactionProfiles == null)
+        {
+          return 0;
+        }
+        if (!interactionProfiles.ContainsKey(parent.Name))
+        {
+          return 0;
+        }
+        var profileData = interactionProfiles[parent.Name];
+        var profile = new Profile(profileData);
+        return profile[key];
+      }
     }
 
     #endregion
